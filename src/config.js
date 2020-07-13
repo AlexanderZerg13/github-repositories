0dbd23b38d6f9f75b905686289ce7c9e68ff5687
Alexandr Pilipenko <pilin94@gmail.com>
2020-07-13 09:29:40 +0300

    Fix access token

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     endpoint: 'https://api.github.com/graphql',
-    accessToken: '20e971f1e3b53345df184393f9c9cb940472e871'
+    accessToken: 'cacb4d21fa3b0ceeb198fbf997b68ffaeae57f1f'
 }
